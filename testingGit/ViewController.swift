7e73d4ec8a3e30bed736e42aec805e3010f431c8
petersoeun <petersoeun@Macbooks-MacBook-Pro.local>
2017-11-27 19:10:53 +0700

    Initial Commit

---
+++ testingGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingGit
+//
+//  Created by petersoeun on 11/27/17.
+//  Copyright © 2017 RoboCam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
